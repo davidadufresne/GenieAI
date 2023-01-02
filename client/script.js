1c79ab0fb524a9e23108a06d53f8e6ed221b7bb5
davidadufresne <118769743+davidadufresne@users.noreply.github.com>
2023-01-02 18:11:18 -0500

    morechanges

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ const handleSubmit = async (e) => {
 
 // fetch data from server - bot's response
 
-const response = await fetch('https://genie-the-mentor.onrender.com',{
+const response = await fetch('http://localhost:5000',{
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
